f41600f8ea933c0e7c2c046ffdfbd7cbafa7d3f3
mayank4244 <mayankmishra9413@gmail.com>
2025-06-15 00:18:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ function startDownload(videoId, url) {
 
   // yt-dlp download best audio only
   const ytdlp = spawn('yt-dlp', ['--cookies', '/etc/secrets/cookies.txt',
-                                 '--no-write-cookies',
                                  '-f', 'bestaudio[ext=webm]/bestaudio/best', 
                                  '-o', filePath, 
                                  url]);
